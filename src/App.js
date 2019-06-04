eb73a0d40b1cbc1af8a37f11731c7fee98955807
Chi-Heng Huang <polozero1@yahoo.com.tw>
2019-06-04 23:55:17 +0800

    add header layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import './assets/style/normalize.css';
+import './assets/style/custom.scss';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Header />;
+  }
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Image, Menu, Form, Input, Icon } from 'semantic-ui-react';
+import './Header.scss';
+import logo from '../../assets/img/logo.jpg';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <Menu borderless className="top-menu" fixed="top">
+        <Menu.Item header className="logo">
+          <Image src={logo} size="tiny" />
+        </Menu.Item>
+        <Menu.Menu className="nav-container">
+          <Menu.Item className="search-input">
+            <Form>
+              <Form.Field>
+                <Input placeholder="Search" size="small" action="Go" />
+              </Form.Field>
+            </Form>
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Icon className="header-icon" name="video camera" size="large" />
+          </Menu.Item>
+          <Menu.Item>
+            <Icon className="header-icon" name="grid layout" size="large" />
+          </Menu.Item>
+          <Menu.Item>
+            <Icon className="header-icon" name="chat" size="large" />
+          </Menu.Item>
+          <Menu.Item>
+            <Icon className="header-icon" name="alarm" size="large" />
+          </Menu.Item>
+          <Menu.Item name="avatar">
+            <Image src="http://via.placeholder.com/80x80" avatar />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
